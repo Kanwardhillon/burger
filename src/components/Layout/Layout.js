a1fc155597f34605d7b54ae30ae118c5941e9398
Kanwarpal <dhillonkanwarpal75@gmail.com>
2022-07-02 00:28:00 -0400

    functionality to Side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Auxil from "../../hoc/Auxil"
 import classes from "./Layout.module.css"
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => {
-  return (
-    <Auxil>
+class Layout extends Component {
+
+  state = {
+    showSideDrawer: true
+  }
+
+  sideDrawerClosedHandler = () =>{
+     this.setState({showSideDrawer: false})
+  }
+
+  render(){
+    return(
+      <Auxil>
       <Toolbar/>
-    <div>Toolbar, Sidebar, Backdrop</div>
+    <SideDrawer closed={this.sideDrawerClosedHandler}
+    open={this.state.showSideDrawer}/>
     <main className={classes.Content}>
-        {props.children}
+        {this.props.children}
     </main>
     </Auxil>
-  )
+    )
+  }
 }
 
-export default layout;
+export default Layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import classes from "./Toolbar.module.css"
+import Logo from "../../Logo/Logo"
+import NavigationItems from '../NavigationItems/NavigationItems'
 
 const Toolbar = (props) => {
   return (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <div>LOGO</div>
-        <nav >
-            ...
+        <div className={classes.Logo}>
+          <Logo/>
+        </div>
+        <nav className={classes.DesktopOnly}>
+           <NavigationItems/>
         </nav>
     </header>
   )
 }
 
 
-export default Toolbar
+export default Toolbar 
